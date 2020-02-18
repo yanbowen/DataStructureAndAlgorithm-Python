157a22c6e30424c207d5030b3183bbdc4dbd33d1
boweny.yan <boweny.yan@qunar.com>
2020-02-18 23:29:52 +0800

    Initial commit

---
+++ LeetCode/001/BinarySearchTree.py
@@ -0,0 +1,16 @@
+from LeetCode.common import TreeNode
+
+
+class BinarySearchTree:
+    def isValidBST(self, root: TreeNode):
+        inorder = self.inorder(root)
+        return inorder == list(sorted(set(inorder)))
+
+    def inorder(self, root):
+        if root is None:
+            return []
+        return self.inorder(root.left) + [root.val] + self.inorder(root.right)
+
+
+if __name__ == '__main__':
+    BinarySearchTree.isValidBST()
