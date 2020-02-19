77386f87af5d9c2c96518069bd91685c7693ddab
boweny.yan <boweny.yan@qunar.com>
2020-02-19 12:56:50 +0800

    add question 1

--- LeetCode/001/BinarySearchTree.py
+++ LeetCode/001/BinarySearchTree.py
@@ -15,3 +15,23 @@ class BinarySearchTree:
 if __name__ == '__main__':
     num = [2, 1] + [3]
 print(num)
+
+
+# 递归方式
+class ValidateBinarySearchTree:
+    def __init__(self):
+        self.prev = None
+
+    def isValidBST(self, root: TreeNode):
+        self.prev = None
+        return self.helper(root)
+
+    def helper(self, root: TreeNode):
+        if root is None:
+            return True
+        if not self.helper(root.left):
+            return False
+        if self.prev and self.prev.val >= root.val:
+            return False
+        self.prev = root
+        return self.helper(root.right)
